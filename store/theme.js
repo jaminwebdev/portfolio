57cd7dbf4524cd657b71bc0ed01d4039ea4e2b31
Jamin <jaminwebdev@gmail.com>
2022-02-19 13:57:17 -0800

    theme store created

---
+++ store/theme.js
@@ -0,0 +1,70 @@
+export const state = () => ({
+  theme: "minimal",
+  viewFade: false,
+});
+
+export const mutations = {
+  SET_THEME(state, newTheme) {
+    state.theme = newTheme;
+  },
+  SET_VIEWFADE(state, viewFade) {
+    state.viewFade = viewFade;
+  },
+};
+
+export const actions = {
+  changeTheme({ commit }, theme) {
+    commit("SET_THEME", theme);
+  },
+  fadeThenChange({ commit, dispatch }, theme) {
+    commit("SET_VIEWFADE", true);
+
+    setTimeout(() => {
+      dispatch("changeTheme", theme);
+      commit("SET_VIEWFADE", false);
+    }, 200);
+  },
+};
+
+export const getters = {
+  getTheme(state) {
+    return state.theme;
+  },
+  getThemeBorderRadius(state) {
+    if (state.theme === "default") {
+      return "0";
+    } else {
+      return "1rem";
+    }
+  },
+  getThemeCards(state) {
+    // default, roundedBg, rounded,
+    let cardType = "default";
+
+    switch (state.theme) {
+      case "default":
+        cardType = "default";
+        break;
+      case "rounded":
+        cardType = "roundedBg";
+        break;
+      case "minimal":
+        cardType = "rounded";
+        break;
+      default:
+        cardType = "default";
+    }
+
+    return cardType;
+  },
+  getThemeStackCards(state) {
+    // bordered, bgVector
+    let cardType = "bordered";
+
+    if (state.theme === "minimal") {
+      cardType = "bgVector";
+    }
+
+    return cardType;
+  },
+};
