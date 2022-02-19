6a8c50c970426d729562e58c15ed8923858c82e6
Jamin <jaminwebdev@gmail.com>
2022-02-19 13:54:57 -0800

    basic styles created

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <h1>Hello</h1>
+  <div class="myContainer">
+    <h1>Primary Heading</h1>
+    <h2>Secondary Heading</h2>
+    <h3>Tertiary Heading</h3>
+    <h4>Last Heading</h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Sem integer vitae
+      justo eget magna fermentum iaculis. Nisi lacus sed viverra tellus in hac.
+      Massa massa ultricies mi quis hendrerit dolor magna eget. Nibh ipsum
+      consequat nisl vel pretium lectus quam. Nunc scelerisque viverra mauris in
+      aliquam sem fringilla ut. Massa eget egestas purus viverra accumsan in
+      nisl nisi scelerisque. Hendrerit gravida rutrum quisque non. Diam donec
+      adipiscing tristique risus nec feugiat in fermentum posuere. Posuere ac ut
+      consequat semper viverra nam libero. Id venenatis a condimentum vitae.
+      Ipsum nunc aliquet bibendum enim facilisis gravida neque. Amet consectetur
+      adipiscing elit duis tristique sollicitudin nibh sit amet.
+    </p>
+    <p>
+      In ante metus dictum at tempor commodo ullamcorper a. Nunc congue nisi
+      vitae suscipit tellus mauris. Volutpat consequat mauris nunc congue nisi
+      vitae suscipit. Dolor sit amet consectetur adipiscing elit pellentesque
+      habitant. Odio ut sem nulla pharetra. Turpis massa sed elementum tempus
+      egestas sed sed risus pretium. At ultrices mi tempus imperdiet nulla
+      malesuada pellentesque elit. Consectetur lorem donec massa sapien
+      faucibus. Id aliquet lectus proin nibh nisl condimentum id venenatis a.
+      Mattis rhoncus urna neque viverra justo nec ultrices dui. Sed cras ornare
+      arcu dui vivamus arcu. Sit amet aliquam id diam maecenas ultricies mi.
+      Eget gravida cum sociis natoque penatibus et magnis dis parturient.
+      Pulvinar neque laoreet suspendisse interdum consectetur libero id
+      faucibus. Faucibus scelerisque eleifend donec pretium vulputate. Pretium
+      fusce id velit ut tortor. Sed arcu non odio euismod. Metus aliquam
+      eleifend mi in nulla posuere sollicitudin aliquam. Nisl nunc mi ipsum
+      faucibus vitae aliquet. Turpis in eu mi bibendum neque egestas.
+    </p>
+  </div>
 </template>
 
 <script>
