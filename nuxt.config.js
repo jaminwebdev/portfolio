7c135e84ec486c37a1a4de0c23edb5eb905d3942
Jamin <jaminwebdev@gmail.com>
2022-02-19 13:30:32 -0800

    dependencies set up

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,83 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: "portfolio",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ["@/assets/css/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ["~/plugins/vue-mask.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      "~/components",
+      "~/components/layout",
+      "~/components/cards",
+      "~/components/forms",
+      "~components/tabs",
+      "~components/sections",
+    ],
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: ["@nuxtjs/google-fonts", "@braid/vue-formulate/nuxt"],
+
+  // Google Fonts
+  googleFonts: {
+    families: {
+      Poppins: [300, 400, 500, 600, 700, 900],
+    },
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/buefy
-    'nuxt-buefy',
+    ["nuxt-buefy", { css: false }],
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content",
+    "@nuxt/image",
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+  image: {
+    domains: ["imgur.com", "i.imgur.com", "https://nuxtjs.org"],
   },
 
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
   // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
+  // content prism theme
+  content: {
+    markdown: {
+      prism: {
+        theme: "prism-themes/themes/prism-lucario.css",
+      },
+    },
+    liveEdit: false,
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
-}
+    babel: {
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
+    },
+  },
+};
