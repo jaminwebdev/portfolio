c4b34118fdcd76aa047134d4c357eeb777f50253
Jamin <jaminwebdev@gmail.com>
2022-02-19 22:27:56 -0800

    app icon and cards created

--- components/cards/OverlapCard.vue
+++ components/cards/OverlapCard.vue
@@ -1,209 +1,209 @@
 <template>
-    <div class="overlapCard">
-        <div class="overlapCard__imgContainer" :style="bgImgStyles"></div>
-        <div class="overlapCard__card" :style="cardStyles">
-            <p class="overlapCard__featuredLabel">Featured</p>
-            <div class="overlapCard__tagContainer">
-                <div class="overlapCard__iconsContainer">
-                    <slot name="icons"></slot>
-                </div>
-                <div class="overlapCard__tagLink">
-                    <slot name="tag"></slot>
-                </div>
-            </div>
-            <div class="overlapCard__contentContainer">
-                <div class="overlapCard__headings">
-                    <slot name="heading"></slot>
-                    <slot name="subtitle"></slot>
-                    <div class="overlapCard__meta">
-                        <slot name="meta"></slot>
-                    </div>
-                </div>
-                <div class="overlapCard__body">
-                    <slot name="body"></slot>
-                </div>
-                <div class="overlapCard__btns">
-                    <slot name="btns"></slot>
-                </div>
-            </div>
+  <div class="overlapCard">
+    <div class="overlapCard__imgContainer" :style="bgImgStyles"></div>
+    <div class="overlapCard__card" :style="cardStyles">
+      <p class="overlapCard__featuredLabel">Featured</p>
+      <div class="overlapCard__tagContainer">
+        <div class="overlapCard__iconsContainer">
+          <slot name="icons"></slot>
         </div>
+        <div class="overlapCard__tagLink">
+          <slot name="tag"></slot>
+        </div>
+      </div>
+      <div class="overlapCard__contentContainer">
+        <div class="overlapCard__headings">
+          <slot name="heading"></slot>
+          <slot name="subtitle"></slot>
+          <div class="overlapCard__meta">
+            <slot name="meta"></slot>
+          </div>
+        </div>
+        <div class="overlapCard__body">
+          <slot name="body"></slot>
+        </div>
+        <div class="overlapCard__btns">
+          <slot name="btns"></slot>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        borderRadius: {
-            type: Boolean,
-            default: false,
-        },
-        bgImg: {
-            type: String,
-            default: "https://i.imgur.com/F24Y91W.jpg",
-        },
+  props: {
+    borderRadius: {
+      type: Boolean,
+      default: false,
     },
-    computed: {
-        bgImgStyles() {
-            return {
-                background: `url(${this.$props.bgImg})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center center",
-                borderRadius: this.borderRadius ? "1.5rem" : "0",
-            }
-        },
-        cardStyles() {
-            return {
-                borderRadius: this.borderRadius ? "1.5rem" : "0",
-            }
-        },
+    bgImg: {
+      type: String,
+      default: "https://i.imgur.com/F24Y91W.jpg",
     },
+  },
+  computed: {
+    bgImgStyles() {
+      return {
+        background: `url(${this.$props.bgImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        borderRadius: this.borderRadius ? "1.5rem" : "0",
+      }
+    },
+    cardStyles() {
+      return {
+        borderRadius: this.borderRadius ? "1.5rem" : "0",
+      }
+    },
+  },
 }
 </script>
 
 <style lang="scss">
 .overlapCard {
-    width: 100%;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: 1fr 4fr 1fr;
+
+  &__imgContainer {
+    grid-row: 1/-1;
+    grid-column: 1 / span 3;
+    background-size: cover;
+    background-position: center center;
+
+    @media only screen and (max-width: 100px) {
+      grid-column: 1 / span 4;
+    }
+  }
+
+  &__card {
+    grid-row: 2/3;
+    grid-column: 3/-1;
+    background: #fff;
+    padding: 30px 40px;
+    box-shadow: 3px 4px 15px rgba(0, 0, 0, 0.08);
+    position: relative;
 
     display: grid;
-    grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: 1fr 4fr 1fr;
+    grid-template-columns: repeat(2, 1fr);
 
-    &__imgContainer {
-        grid-row: 1/-1;
-        grid-column: 1 / span 3;
-        background-size: cover;
-        background-position: center center;
+    @media only screen and (max-width: 100px) {
+      grid-column: 4 / -1;
+    }
+  }
 
-        @media only screen and (max-width: 68.75em) {
-            grid-column: 1 / span 4;
-        }
+  &__featuredLabel {
+    position: absolute;
+    right: 0;
+    top: -32px;
+    background: var(--primary-color);
+    color: #fff;
+    padding: 5px 15px;
+    font-size: 14px;
+  }
+
+  &__tagContainer {
+    grid-column: 1/-1;
+
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+
+  &__iconsContainer {
+    display: flex;
+  }
+
+  &__tagLink {
+    display: grid;
+    justify-items: end;
+
+    & svg {
+      width: 35px;
+      transition: all 0.2s;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  &__contentContainer {
+    grid-column: 1/-1;
+  }
+
+  &__headings {
+    & h2,
+    & h3,
+    & h4 {
+      text-align: left;
+    }
+    margin-bottom: 15px;
+
+    & a h3,
+    & a h4 {
+      transition: transform 0.2s ease-out;
+      display: inline-block;
+
+      &:hover {
+        transform: scale(1.01);
+        color: var(--primary-color);
+      }
+    }
+  }
+
+  &__meta {
+    display: grid;
+    grid-gap: 15px;
+    grid-auto-flow: column;
+    grid-auto-columns: max-content;
+    margin-bottom: 20px;
+
+    @media only screen and (max-width: 1000px) {
+      display: flex;
+      flex-wrap: wrap;
     }
 
-    &__card {
-        grid-row: 2/3;
-        grid-column: 3/-1;
-        background: #fff;
-        padding: 3rem 4rem;
-        box-shadow: 3px 4px 15px rgba(0, 0, 0, 0.08);
-        position: relative;
-
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-
-        @media only screen and (max-width: 68.75em) {
-            grid-column: 4 / -1;
-        }
+    & a,
+    & span {
+      color: var(--primary-color);
+      font-size: 16px;
     }
 
-    &__featuredLabel {
-        position: absolute;
-        right: 0;
-        top: -3.2rem;
-        background: var(--primary-color);
-        color: #fff;
-        padding: 0.5rem 1.5rem;
-        font-size: 1.4rem;
+    & a {
+      margin-bottom: 0;
+      transition: transform 0.1s ease-out;
+
+      &:hover {
+        color: var(--primary-color-dark);
+        transform: scale(1.05);
+      }
     }
 
-    &__tagContainer {
-        grid-column: 1/-1;
+    & > span {
+      display: grid;
+      justify-items: center;
+      align-items: center;
+      grid-auto-flow: column;
+      grid-gap: 5px;
 
-        display: grid;
-        grid-template-columns: 2fr 1fr;
-    }
-
-    &__iconsContainer {
-        display: flex;
-    }
-
-    &__tagLink {
-        display: grid;
-        justify-items: end;
-
-        & svg {
-            width: 3.5rem;
-            transition: all 0.2s;
-
-            &:hover {
-                transform: scale(1.1);
-            }
-        }
-    }
-
-    &__contentContainer {
-        grid-column: 1/-1;
-    }
-
-    &__headings {
-        & h2,
-        & h3,
-        & h4 {
-            text-align: left;
-        }
-        margin-bottom: 1.5rem;
-
-        & a h3,
-        & a h4 {
-            transition: transform 0.2s ease-out;
-            display: inline-block;
-
-            &:hover {
-                transform: scale(1.01);
-                color: var(--primary-color);
-            }
-        }
-    }
-
-    &__meta {
-        display: grid;
-        grid-gap: 1.5rem;
-        grid-auto-flow: column;
+      @media only screen and (max-width: 1000px) {
         grid-auto-columns: max-content;
-        margin-bottom: 2rem;
-
-        @media only screen and (max-width: 56.25rem) {
-            display: flex;
-            flex-wrap: wrap;
-        }
-
-        & a,
-        & span {
-            color: var(--primary-color);
-            font-size: 1.6rem;
-        }
-
-        & a {
-            margin-bottom: 0;
-            transition: transform 0.1s ease-out;
-
-            &:hover {
-                color: var(--primary-color-dark);
-                transform: scale(1.05);
-            }
-        }
-
-        & > span {
-            display: grid;
-            justify-items: center;
-            align-items: center;
-            grid-auto-flow: column;
-            grid-gap: 0.5rem;
-
-            @media only screen and (max-width: 56.25rem) {
-                grid-auto-columns: max-content;
-                grid-gap: 1rem;
-            }
-        }
+        grid-gap: 10px;
+      }
     }
+  }
 
-    &__body {
-        margin-bottom: 3.5rem;
-    }
+  &__body {
+    margin-bottom: 35px;
+  }
 
-    &__btns {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 4rem;
-    }
+  &__btns {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 40px;
+  }
 }
 </style>
